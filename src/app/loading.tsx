aab18a796267d19da8d34c2716e1d79895454e60
root <root@Apollo.>
2025-06-30 22:45:26 -0500

    Replace variant with static dashboard showcasing all 10 Lotus variants

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingSpinner from '@/components/LoadingSpinner';
-
-export default function Loading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <LoadingSpinner size="large" message="Loading..." />
-    </div>
-  );
-}
